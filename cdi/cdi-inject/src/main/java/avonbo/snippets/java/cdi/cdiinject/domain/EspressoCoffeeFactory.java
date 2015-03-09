2ea0964e7bf119e29e336d652d67e3fa5649ae0b
avonbo <alexander@vonboguszewski.name>
2015-03-09 12:12:33 +0100

    Revert "add cdi snippets"
    This reverts commit c916fd596f7d2e5987dcfb12b2b0430f390749b4.

--- cdi/cdi-inject/src/main/java/avonbo/snippets/java/cdi/cdiinject/domain/EspressoCoffeeFactory.java
+++
@@ -1,9 +0,0 @@
-package avonbo.snippets.java.cdi.cdiinject.domain;
-
-public class EspressoCoffeeFactory implements CoffeeFactory{
-
-	public Coffee createCoffee() {
-		return new Coffee("espresso");
-	}
-
-}
--- cdi/cdi-named-inject/src/main/java/avonbo/snippets/java/cdi/cdinamedinject/domain/CoffeeFactory.java
+++
@@ -1,7 +0,0 @@
-package avonbo.snippets.java.cdi.cdinamedinject.domain;
-
-public interface CoffeeFactory {
-
-	Coffee createCoffee();
-
-}
