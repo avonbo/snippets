e3a932fe3695a31dac6892195809061f3b6a18f8
avonbo <alexander@vonboguszewski.name>
2015-08-28 17:04:05 +0200

    Add Test

--- actor/src/test/java/avonbo/snippets/java/actor/ReceivedMessageCheckingBehaviour.java
+++ actor/src/test/java/avonbo/snippets/java/actor/ReceivedMessageCheckingBehaviour.java
@@ -1,6 +1,6 @@
 package avonbo.snippets.java.actor;
 
-public class ReceivedMessageCheckingBehaviour implements Behavior<String> {
+public class ReceivedMessageCheckingBehaviour implements Behavior<MyMessage> {
 
 	private String myMessage;
 	private Exception myException;
@@ -20,10 +20,16 @@ public class ReceivedMessageCheckingBehaviour implements Behavior<String> {
 	}
 
 	@Override
-	public boolean receive(String msg) {
-		System.out.println("Receive myMessage=" + msg);
-		myMessage = msg;
-		return false;
+	public boolean receive(MyMessage msg) {
+		try{
+			System.out.println("Receive myMessage=" + msg);
+			myMessage = msg.getMessage();
+			msg.setMessage(myMessage + " > Got on " + System.currentTimeMillis());
+		} catch (final Exception e) {
+			myException = e;
+			return false;
+		}
+		return true;
 	}
 
 }
--- actor/src/test/java/avonbo/snippets/java/actor/TestSending.java
+++ actor/src/test/java/avonbo/snippets/java/actor/TestSending.java
@@ -2,6 +2,9 @@ package avonbo.snippets.java.actor;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Test;
 
 import avonbo.snippets.java.actor.actorimpl.disruptor.DisruptorSEActor;
@@ -12,14 +15,42 @@ public class TestSending {
 	public void testSend() {
 		final ReceivedMessageCheckingBehaviour behaviour = new ReceivedMessageCheckingBehaviour();
 
-		final Actor<String> a = new DisruptorSEActor<>(behaviour);
+		final Actor<MyMessage> a = new DisruptorSEActor<>(behaviour);
+
+		final MyMessage msg = new MyMessage();
+		msg.setMessage("initial");
+
 		try {
-			a.publish("testing");
+			a.publish(msg);
 			Thread.sleep(1000);
 		} catch (final InterruptedException e) {
 		}
 
-		assertEquals("testing", behaviour.getMessage());
+		assertEquals("initial", behaviour.getMessage());
+	}
+
+	@Test
+	public void testSendTwoReceiver() {
+		final ReceivedMessageCheckingBehaviour behaviour1
+		= new ReceivedMessageCheckingBehaviour();
+		final ReceivedMessageCheckingBehaviour behaviour2
+		= new ReceivedMessageCheckingBehaviour();
+
+		final List<ReceivedMessageCheckingBehaviour> behaviors
+		= Arrays.asList(behaviour1, behaviour2);
+
+		final Actor<MyMessage> a = new DisruptorSEActor(behaviors);
+
+		final MyMessage msg = new MyMessage();
+		msg.setMessage("initial");
+
+		try {
+			a.publish(msg);
+			Thread.sleep(1000);
+		} catch (final InterruptedException e) {
+		}
+
+		assertEquals("initial", behaviour1.getMessage());
 	}
 
 }
